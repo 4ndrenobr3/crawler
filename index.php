7cfba18f2b01f4349069dde7764176cc53cc3525
4ndrenobr3 <nobre4ndr3@gmail.com>
2021-01-07 18:12:47 -0300

    Iniciado a criacao do crawler

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require_once __DIR__ . './vendor/autoload.php';
 use App\Crawler;
 
 $crawler = new Crawler();
-echo $crawler->index('Hi Lorena!');
+$crawler->index();
--- src/Crawler.php
+++ src/Crawler.php
@@ -1,10 +1,36 @@
 <?php
 namespace App;
 
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler as DomCrawler;
+
 class Crawler
 {
-    public function index($msg)
+    public function index()
     {
-        return $msg;
+        $client    = new Client();
+        $response  = $client->get('https://www.carrosnaserra.com.br/capa');
+        $html      = $response->getBody()->getContents();
+
+        $crawler   = new DomCrawler($html);
+
+        $data = $crawler->filter('div[class="box-estoque-capas-normal"]')->each(function ($contentContainer) {
+
+            $Title = $contentContainer->filter('span[class="modelo-capas"]')->text();
+            $brand = $contentContainer->filter('span[class="marca-ano-capas"]')->text();
+            $year  = $contentContainer->filter('span[class="anos-capas"]')->text();
+            $price = $contentContainer->filter('span[class="valor-capas"]')->text();
+            $photo = $contentContainer->filter('a')->link();
+
+            return [
+                'title' => $Title,
+                'brand' => $brand,
+                'year'  => $year,
+                'price' => $price,
+                'photo' => $photo
+            ];
+        });
+
+        var_dump($data);
     }
 }
